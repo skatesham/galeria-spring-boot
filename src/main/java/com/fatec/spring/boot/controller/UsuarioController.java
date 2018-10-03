dc314ee652555ff23e52df71964f344d2a69239e
skatesham <shamvinicius@gmail.com>
2018-10-03 00:07:02 -0300

    "Fixing CORS

--- src/main/java/com/fatec/spring/boot/controller/UsuarioController.java
+++ src/main/java/com/fatec/spring/boot/controller/UsuarioController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +18,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonView;
-import com.fatec.spring.boot.model.EnumPapel;
 import com.fatec.spring.boot.model.Usuario;
 import com.fatec.spring.boot.service.UsuarioService;
 import com.fatec.spring.boot.view.View;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
@@ -38,10 +39,9 @@ public class UsuarioController {
 	@RequestMapping(value = "/getAll", method = RequestMethod.GET)
 	@JsonView(View.UsuarioFull.class)
 	@Transactional
-	public ResponseEntity<Collection<Usuario>> getAll() {
+	public ResponseEntity<Collection<Usuario>> getAll( HttpServletRequest request, HttpServletResponse response) {
 
 		Set<Usuario> usuarios = usuarioService.lerTodos();
-
 		return new ResponseEntity<Collection<Usuario>>(usuarios, HttpStatus.OK);
 	}
 	
